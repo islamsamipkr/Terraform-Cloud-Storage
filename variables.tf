a25e8f48c46265e7c0963259b4bcbfe72d5e5ef9
Sami Islam <islam.samipkr@gmail.com>
2023-03-18 10:12:08 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -19,7 +19,9 @@ variable "project_id" {
   type        = string
   default     = "avian-amulet-378416"
 }
-
+variable "Google_Credentials" { 
+  type = string 
+}
 variable "prefix" {
   description = "Prefix used to generate the bucket name."
   type        = string
